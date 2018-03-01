190da24636fd1bf9006015de0c0d6291003a4149
Skanarky <kutkurov@gmail.com>
2018-02-28 17:17:12 -0700

    Added final ver of Terminal Practice and new assignment directories - CIA I and II

--- exercises/terminal-practice/dir-two/main.js
+++ exercises/terminal-practice/dir-two/main.js
@@ -1,8 +1,8 @@
-/* let hello = "Hello World";
+let hello = "Hello World";
 let h1 = document.getElementById("append");
 const helloCreate = document.createElement("h1");
 helloCreate.innerHTML = hello;
 h1.appendChild(helloCreate); 
 
 let h2 = document.getElementById("append2");
-h2.textContent = "Hello Guys"; */
+h2.textContent = "Hello Guys";
