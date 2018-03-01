3602ff683444e8854467fb1a2c41c646fa9b2b62
Skanarky <kutkurov@gmail.com>
2018-03-01 09:06:40 -0700

    Having fun printing with different functions

--- exercises/terminal-practice/dir-two/main.js
+++ exercises/terminal-practice/dir-two/main.js
@@ -1,8 +1,36 @@
-let hello = "Hello World";
-let h1 = document.getElementById("append");
-const helloCreate = document.createElement("h1");
-helloCreate.innerHTML = hello;
-h1.appendChild(helloCreate); 
+// Calling fns AFTER THEIR CREATION, using innerHTML
 
-let h2 = document.getElementById("append2");
-h2.textContent = "Hello Guys";
+function printHelloOne() {
+    let hello = "Hello World 1";
+    let h1 = document.getElementById("append");
+    const helloCreate = document.createElement("h1");
+    helloCreate.innerHTML = hello;
+    h1.appendChild(helloCreate);
+};
+
+printHelloOne();
+
+function printHelloTwo() {
+    var hello = "Hello Test 2";
+    var h2 = document.getElementById("append2");
+    h2.innerHTML = "<h2>" + hello + "</h2>";
+};
+
+printHelloTwo();
+
+// Calling fns BEFORE THEIR CREATION, using text fns
+printHelloThree();
+
+function printHelloThree() {
+    let h3 = document.getElementById("append3");
+    h3.textContent = "Hello Guys 3";
+};
+
+printHelloFour();
+
+function printHelloFour() {
+    let h4 = document.getElementById("append5");
+    let newLine = document.getElementById("append4");
+    newLine.innerHTML = "<br />";
+    h4.innerText = "Test Inner Text md 4";
+};
