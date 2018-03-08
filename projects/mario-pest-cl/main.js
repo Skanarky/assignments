6660b4ece12e355da683feb3eb28e4c76bcef9b2
Skanarky <kutkurov@gmail.com>
2018-03-08 02:29:42 -0700

    Almost finished Mario Pest prj

--- projects/mario-pest-cl/main.js
+++ projects/mario-pest-cl/main.js
@@ -1,38 +1,17 @@
-// ADD
-
-var add1 = document.getElementById("add1");
-var add2 = document.getElementById("add2");
-var add = document.getElementById("add");
-var addRes = document.getElementById("addres");
-
-add.addEventListener("click", adding);
-
-function adding (event) {
-    addRes.innerHTML = Number(add1.value) + Number(add2.value);
-}
-
-// SUBTR
-
-var subt1 = document.getElementById("subt1");
-var subt2 = document.getElementById("subt2");
-var subtr = document.getElementById("subtr");
-var subtrres = document.getElementById("subtrres");
-
-subtr.addEventListener("click", subtrNo);
-
-function subtrNo (event) {
-    subtrres.innerHTML = Number(subt1.value) - Number(subt2.value);
-}
-
 // MULT
 
-var mult1 = document.getElementById("mult1");
-var mult2 = document.getElementById("mult2");
+var goomba5 = document.getElementById("mult1");
+var bobomb7 = document.getElementById("mult2");
+var cheep11 = document.getElementById("mult3");
 var mult = document.getElementById("mult");
-var multres = document.getElementById("multres");
+var result = document.getElementById("result");
 
+goomba5.focus();
 mult.addEventListener("click", multipl);
 
 function multipl (event) {
-    multres.innerHTML = Number(mult1.value) * Number(mult2.value);
+    var nGoo = Number(goomba5.value) * 5;
+    var nBob = Number(bobomb7.value) * 7;
+    var nCheep = Number(cheep11.value) * 11;
+    result.innerHTML = nGoo + nBob + nCheep;
 }
