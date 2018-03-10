70e1d0b02a9312d980cfa795963fe0d46bb6ca6a
Skanarky <kutkurov@gmail.com>
2018-03-09 18:02:27 -0700

    Finished Node Calc prj

--- exercises/node-calc/calc.js
+++ exercises/node-calc/calc.js
@@ -2,36 +2,49 @@ var ask = require("readline-sync");
 
 // prompts
     //get number
+    // var num1 = ask.question("Pick a whole number?");
+
+//get another number
+    // var num2 = ask.question("Pick another whole number?");
+
+//choose operation/function
 var num1 = ask.question("Pick a whole number?");
-    
-    //get another number
 var num2 = ask.question("Pick another whole number?");
-
-    //choose operation/function
-
-var operations = ["+", "-", "*", "/"];
+var operations = ["add", "subtr", "mult", "divid"];
 var indexFrom = ask.keyInSelect(operations, "Choose an operation to perform");
 
+if (operations[indexFrom] === "add") {
+    console.log(Number(num1) + Number(num2));
+} else if (operations[indexFrom] === "subtr") {
+    console.log(num1 - num2);
+} else if (operations[indexFrom] === "mult") {
+    console.log(num1 * num2);
+} else if (operations[indexFrom] === "divid") {
+    console.log(num1 / num2);
+}   else {
+    console.log("Thanks For Trying Our Calculator!\nBye!");
+}
+
 // chooser fn
-    // pick numbers and operators
+// pick numbers and operators
 
 
 // selects which fn to use and call it by choosing the number as arguments
 
 // create fn-s for the 4 operations: + - / * arithmetic fn-s
 
-function add (a, b) {
-    return a + b;
-}
+// function add (a, b) {
+//     return a + b;
+// }
 
-function subtr (a, b) {
-    return a - b;
-}
+// function subtr (a, b) {
+//     return a - b;
+// }
 
-function mult (a, b) {
-    return a * b;
-}
+// function mult (a, b) {
+//     return a * b;
+// }
 
-function divi (a, b) {
-    return a / b;
-}
+// function divi (a, b) {
+//     return a / b;
+// }
