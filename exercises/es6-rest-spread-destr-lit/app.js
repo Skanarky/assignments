0882991069587521bfb9151dee19ca34555cf6b9
Skanarky <kutkurov@gmail.com>
2018-03-27 08:44:56 -0600

    Finished ES6 Rest Spread Etc. ex

--- exercises/es6-rest-spread-destr-lit/app.js
+++ exercises/es6-rest-spread-destr-lit/app.js
@@ -1,11 +1,11 @@
-//---------
+//--------- REST
 function collectAnimals(...animals) {
     return animals;
 }
 
 // console.log(collectAnimals("dog", "mouse"));
 
-//---------
+//--------- OBJECT Literals
 
 const fruit = ["apple", "pear"];
 const sweets = ["cake", "pie"];
@@ -19,7 +19,7 @@ const food = {
 
 // console.log(food);
 
-//---------
+//--------- DESTRUCTING / DECONSTRUCTING Object
 
 const vacation = {
     location: "Burly Idaho",
@@ -33,12 +33,12 @@ const {
 
 // console.log(`We're going to have a good time in ${location} for ${duration}`);
 
-//---------
+//--------- DESTRUCTING / DECONSTRUCTING Array I
 
 // const [ firstItem ] = items;  
 // const [ , secondItem ] = items;
 
-//---------
+//--------- DESTRUCTING / DECONSTRUCTING Array II
 
 const favoriteActivitiesInOrder = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
 
@@ -46,14 +46,118 @@ const [firstFav, secondFav, thirdFav] = favoriteActivitiesInOrder;
 
 // console.log(`My top three favorite activities are: ${firstFav}, ${secondFav} and ${thirdFav}`);  
 
-//---------
 
-function combineAnimals(arr1, arr2, ...arrays) {
-    return [[ ...arr1, ...arr2, ], ...arrays ];
+//--------- SPREAD
+
+function combineAnimals(arr1, arr2, arr3) {
+    return [...arr1, ...arr2, ...arr3];
+}
+
+// const realAnimals = ["dog", "cat", "mouse"];
+// const magicalAnimals = ["jackolope"];
+// const mysteriousAnimals = ["platypus"];
+
+// console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals));
+
+
+//--------- REST + SPREAD... but I still need to LOOP --> so REDUCE or forEach... etc...
+
+function combineAnimals(...arr) {
+    return arr.reduce((total, innerArr) => [...total, ...innerArr]);
 }
 
 const realAnimals = ["dog", "cat", "mouse"];
 const magicalAnimals = ["jackolope"];
 const mysteriousAnimals = ["platypus"];
 
-console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals));
+// console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals, [1, true, 4, false], ["a", {propOne: 1, propTwo: "two"}]));
+
+
+//--------- MAKING IT ES6xy I
+
+// function product(a, b, c, d, e) {
+//     var numbers = [a, b, c, d, e];
+
+//     return numbers.reduce(function (acc, number) {
+//         return acc * number;
+//     }, 1)
+// };
+
+function product(...num) {
+    return num.reduce((acc, number) => acc * number, 1)
+}
+
+// console.log(product(2, 3, 5));
+
+
+//--------- MAKING IT ES6xy II
+
+function unshift(array, ...inpSomething) {
+    return [inpSomething, array].reduce((total, innerArr) => [...total, ...innerArr]);
+}
+
+// console.log(unshift(["12", true, 1, 2], 2, "big", false));
+
+
+//--------- OBJECT LITERALS and DECONSTRUCTING / DESTRUCTING
+
+//ES6
+const populatePeople = (names) => names.map((name) => {
+    name = name.split(" ");
+
+    // console.log(name);
+
+    // My code below:
+
+    let [firstName] = name;
+    let [, lastName] = name;
+
+    return {
+        firstName,
+        lastName
+    }
+});
+
+console.log(populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"]));
+
+//ES5 + ES6
+// function populatePeople(names) {
+//     return names.map(function (name) {
+//         name = name.split(" ");
+        
+//         // console.log(name);
+        
+//         // My code below:
+
+//         let [firstName] = name;
+//         let [, lastName] = name;
+
+//         return {
+//             firstName,
+//             lastName
+//         }
+//     });
+// }
+
+// console.log(populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"]));
+
+// ES5 mainly
+// function populatePeople(names) {
+//     return names.map(function (name) {
+//         name = name.split(" ");
+        
+//         // console.log(name);
+        
+//         // My code below:
+
+//         var firstName = name[0];
+//         var lastName = name[1];
+
+//         return {
+//             firstN: firstName,
+//             lastN: lastName
+//         }
+//     });
+// }
+
+// console.log(populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"]));
