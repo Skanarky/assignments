17960d4ee209b106481090673c0d4ea074d136f3
Skanarky <kutkurov@gmail.com>
2018-03-31 11:59:12 -0600

    Reorganised and NEW React folder; Created React 'src-template' folder

---
+++ exercises/react/src-template/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Compon from "./Compon/Compon";
+
+const App = () => {
+    return (
+        <div>
+            {/* tags and/or comp and/or destructing data + comp HERE */}
+        </div>
+    )
+}
+
+export default App;
---
+++ exercises/react/src-template/App/Compon/Compon.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Compon = (props) => {
+    return (
+        <div>
+            {/* tags and/or comp and/or destructing data + comp HERE */}
+        </div>
+    )
+}
+
+export default Compon;
