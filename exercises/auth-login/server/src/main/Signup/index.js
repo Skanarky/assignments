59016de1f819cad60af66b73d995805a781c174b
Skanarky <kutkurov@gmail.com>
2018-05-09 09:44:40 -0600

    added auth ex

---
+++ exercises/auth-login/server/src/main/Signup/index.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import SignupForm from "./SignupForm";
+
+import {connect} from "react-redux";
+import {signup} from "../../redux/auth";
+
+class SignupFormContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inputs: {
+                name: "",
+                username: "",
+                password: ""
+            }
+        }
+    }
+
+    handleChange(e) {
+        e.persist();
+        this.setState(prevState => {
+            return {
+                inputs: {
+                    ...prevState.inputs,
+                    [e.target.name]: e.target.value
+                }
+            }
+        })
+    }
+
+    clearInputs() {
+        this.setState({
+            inputs: {
+                name: "",
+                username: "",
+                password: ""
+            }
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        // This is where we will call our signup function from redux
+        this.props.signup(this.state.inputs);
+        this.clearInputs();
+    }
+
+    render() {
+        const authErrorCode = this.props.authErrCode.signup;
+        let errMsg = "";
+        if(authErrorCode < 500 && authErrorCode > 399){
+            errMsg = "invalid username or password";
+        } else if (authErrorCode > 499){
+            errMsg = "server error";
+        }
+        
+        return (
+            <SignupForm
+                handleChange={this.handleChange.bind(this)}
+                handleSubmit={this.handleSubmit.bind(this)}
+                {...this.state.inputs} errMsg={errMsg} />
+        )
+    }
+}
+
+
+export default connect(state => state.user, {signup})(SignupFormContainer);
+
+
+
