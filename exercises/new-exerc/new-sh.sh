392d2c598a27c922f26c7e6b0655d69f36647777
Skanarky <kutkurov@gmail.com>
2020-01-24 15:27:50 -0700

    shebang and quotes for strings

--- exercises/new-exerc/new-sh.sh
+++ exercises/new-exerc/new-sh.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
+
+echo "- - - - -"
+
+echo "if you change permissions to executable always include the shebang"
+
+echo "otherwise you can explicitly run with 'bash name-of-bash-file'"
+
+echo "- - - - - -" 
+
 date
 
-echo - - - - -
-echo done with date
-echo - - - - -
+echo "- - - - -"
+echo "done with date"
+echo "- - - - -"
 
 cal
 
-echo - - - - -
-echo done with cal
-echo - - - - -
+echo "- - - - -"
+echo "done with cal"
+echo "- - - - -"
