96d06df91f8994812e0e5a117f86bc43655fde29
Skanarky <kutkurov@gmail.com>
2020-01-26 12:54:13 -0700

    more vars + input term exerc

---
+++ exercises/new-exerc/backup-exerc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this, script will backup the Music folder for ANY user of this computer and output it to desktop
+
+user=$(whoami)
+input=/Users/$user/Music
+output=/Users/$user/Desktop/${user}_music_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+echo "- - - - -"
+echo "Start of backing up - $input - with recording script output in file outp-backup.txt"
+echo "- - - - -"
+
+tar -czf  $output $input &> outp-backup.txt
+echo "--- On $(date +%Y-%m-%d_%H%M%S) ---" >> outp-backup.txt
+
+# PRINT  date from file - an example for terminal input from another file
+echo "$(< date-for-bup.txt)"
+
+echo "Backup of - $input - folder for user - $user - done!"
+echo "- - - - -"
+echo "File details:"
+ls -al $output
+echo "- - - - -"
