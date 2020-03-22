e2e42a21494059608333a359bea8823ffe0f97f1
Skanarky <kutkurov@gmail.com>
2020-03-22 14:27:09 -0600

    obj comp

--- exercises/new-exerc/fn-obj-compose.js
+++ exercises/new-exerc/fn-obj-compose.js
@@ -1,6 +1,10 @@
 // "... software development is “the act of breaking a complex problem down into smaller problems, and composing simple solutions to 
 // form a complete solution to the complex problem.”
 
+// all software is composed
+// composed by building blocks
+// building blocks - functions and objects (+ data types and structures)
+
 // FN COMPOSITION
 
 const g = n => n + 1;
@@ -29,4 +33,85 @@ Promise.resolve(5)
 
 // --> 12
 // --> 12
-// --> 12
+
+// OBJECT COMPOSITION
+
+// “Favor object composition over class inheritance” the Gang of Four, 
+// book “Design Patterns: Elements of Reusable Object Oriented Software”
+
+// “In computer science, a composite data type or compound data type is any
+// data type which can be constructed in a program using the programming
+// language’s primitive data types and other composite types. […] The act of
+// constructing a composite type is known as composition.” ~ Wikipedia
+
+// primitive
+const firstName = 'Claude';
+const lastName = 'Debussy';
+
+// composite/compound
+const fullName = {
+    firstName,
+    lastName
+};
+
+// inheritance obj comp
+class Foo {
+    constructor () {
+      this.a = 'a prop'
+    }
+}
+class Bar extends Foo {
+    constructor (options) {
+        super(options);
+        this.b = 'b prop'
+    }
+}
+const myBar = new Bar(); // { a: 'a prop', b: 'b prop' }
+console.log(myBar);
+
+// mixin composition
+// >>> warning for deep clone, can be avoid with deep clone - see further down
+
+const a = {
+    a: 'a prop'
+  };
+const b = {
+    b: 'b prop'
+};
+const c = {
+    c: { d: 0 }
+};
+
+const d = { ...a, ...b , ...c };
+
+console.log(d);
+c.c.d = 3;
+console.log(c);
+console.log(d);
+
+// clone (shallow)
+// >>> warning for deep clone, can be avoid with deep clone - see further down
+
+// const returnedTargetChangesAAsWell = Object.assign(a, b);
+const returnedTarget = Object.assign({}, a, b, c);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+console.log(returnedTarget);
+// returnedTarget.c.d = 3;
+c.c.d = 5;
+console.log(c);
+console.log(returnedTarget);
+console.log(d);
+
+// deep clone
+// returnedTarget2.c.d WON'T change
+
+const returnedTarget2 = JSON.parse(JSON.stringify(returnedTarget));
+
+console.log(returnedTarget);
+console.log(returnedTarget2);
+c.c.d = 7;
+console.log(returnedTarget);
+console.log(returnedTarget2);
