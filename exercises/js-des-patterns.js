2383ad1d12979b3b4c86fa0044fb07079e60923b
Skanarky <kutkurov@gmail.com>
2020-04-25 11:36:14 -0600

    init JS Des Patterns

---
+++ exercises/js-des-patterns.js
@@ -0,0 +1,49 @@
+// THEORY:
+// Categories of Design Patterns (js/ES6)
+// 1. Creational (object creation mechanism) - Constructor Pattern, Factory Pattern, Prototype Pattern, and Singleton Pattern
+// 2. Structural (class and object composition) - Adapter Pattern, Composite Pattern, Decorator Pattern,
+// Façade Pattern, Flyweight Pattern, and Proxy Pattern
+// 3. Behavioral (communication between dissimilar objects) - Chain of Responsibility Pattern, Command Pattern,
+// Iterator Pattern, Mediator Pattern, Observer Pattern, State Pattern, Strategy Pattern, and Template Pattern
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CODE:
+// (includes functions that return examples and logs simulating flash cards in order to help memorizing the material (call a pattern or category
+// function and get the examples and the theory/text))
+
+// - - - Categories - - -
+function creationalCategory() {
+
+}  
+
+// - - - Patterns - - -
+function constructorPattern() {
+
+}
