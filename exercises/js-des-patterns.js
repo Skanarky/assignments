bf56ab5cf5621abd18c0372a660a05d485f2733d
Skanarky <kutkurov@gmail.com>
2020-04-25 12:17:06 -0600

    constructor patt

--- exercises/js-des-patterns.js
+++ exercises/js-des-patterns.js
@@ -1,6 +1,7 @@
-// THEORY:
-// Categories of Design Patterns (js/ES6)
+// THEORY: (remove text eventually - it will all come from the function call)
+// Categories of JS/ES6 Design Patterns ("Design pattern -> general, reusable solution to a commonly occurring problem in software design")
 // 1. Creational (object creation mechanism) - Constructor Pattern, Factory Pattern, Prototype Pattern, and Singleton Pattern
+creationalCategory();
 // 2. Structural (class and object composition) - Adapter Pattern, Composite Pattern, Decorator Pattern,
 // Façade Pattern, Flyweight Pattern, and Proxy Pattern
 // 3. Behavioral (communication between dissimilar objects) - Chain of Responsibility Pattern, Command Pattern,
@@ -35,15 +36,47 @@
 
 
 // CODE:
-// (includes functions that return examples and logs simulating flash cards in order to help memorizing the material (call a pattern or category
+// (includes functions that return logs and examples, simulating flash cards in order to help memorizing the material (call a pattern or category
 // function and get the examples and the theory/text))
 
 // - - - Categories - - -
 function creationalCategory() {
-
-}  
+    console.log("Creational Category:")
+    constructorPattern();
+}
 
 // - - - Patterns - - -
-function constructorPattern() {
+// 1.
 
+function constructorPattern() {
+    console.log("- Constructor Pattern")
+    console.log("* class-based creational design pattern")
+    console.log("* it's a special functions that is used to instantiate new objects with methods and properties defined by that function")
+    console.log("* it's more of a basic language construct than a pattern in most object-oriented languages")
+    console.log("* EXAMPLE: ")
+
+    // Traditional Function-based syntax
+    // function SoccerPlayer(name, position) {
+    //     this.name = name;
+    //     this.position = position;
+
+    //     this.getDetails = function() {
+    //         return this.name + " plays as " + this.position
+    //     }
+    // }
+    
+    // ES6 Class syntax
+    class SoccerPlayer {
+        constructor(name, position) {
+            this._name = name;
+            this._position = position;
+
+            this.getDetails = function() {
+                return `${this._name} plays as ${this._position}`
+            }
+        }
+    }
+
+    const deGea = new SoccerPlayer('De Gea', 'goalkeeper');
+    console.log(deGea.getDetails());
 }
