63e9bb65625e4c8802a63082202ac51c145823d7
Skanarky <kutkurov@gmail.com>
2020-06-13 14:09:10 -0600

    factory pattern for creational ctg

--- exercises/new-exerc/js-des-patterns.js
+++ exercises/new-exerc/js-des-patterns.js
@@ -2,13 +2,20 @@
 
 const callNumCounter = callNum();
 
-// THEORY: (remove it eventually - it will all come from the function call)
-// Categories of JS/ES6 Design Patterns ("Design pattern -> general, reusable solution to a commonly occurring problem in software design")
+// SRC: https://medium.com/better-programming/javascript-design-patterns-25f0faaaa15
+// CREDITS: https://medium.com/@drenther
+
+// THEORY: (remove eventually - it will all come from function calls)
+
+// Categories of JS/ES6 Design Patterns ("Design pattern -> general, reusable solution to a commonly occurring problem in software design"):
+
 // 1. Creational (object creation mechanism) - Constructor Pattern, Factory Pattern, Prototype Pattern, and Singleton Pattern
-categoryFn('Creational')([ constructorPattern ]);
+categoryFn('Creational')([ constructorPattern, factoryPattern ]);
+
 // 2. Structural (class and object composition) - Adapter Pattern, Composite Pattern, Decorator Pattern,
 // Façade Pattern, Flyweight Pattern, and Proxy Pattern
-categoryFn('Structural')([ ()=> console.warn("test STRUCTURAL pattern") ]);
+categoryFn('Structural')([ ()=> console.warn("test function for STRUCTURAL pattern") ]);
+
 // 3. Behavioral (communication between dissimilar objects) - Chain of Responsibility Pattern, Command Pattern,
 // Iterator Pattern, Mediator Pattern, Observer Pattern, State Pattern, Strategy Pattern, and Template Pattern
 categoryFn('Behavioral');
@@ -62,11 +69,11 @@ function separator(sep, times) {
 }
 
 function categoryFn(type) {
-    console.log(`${callNumCounter()}. ${type} Category:`)
+    console.log(`${callNumCounter()}. ${type} Category:`);
     return (patternFnArr) => {
-        
+
         separator("v", 3);
-        
+
         patternFnArr.map(patternFn => {
             patternFn();
             separator("-", 7);
@@ -109,4 +116,70 @@ function constructorPattern() {
 
     const deGea = new SoccerPlayer('De Gea', 'goalkeeper');
     console.log(deGea.getDetails());
+}
+
+// 2.
+
+function factoryPattern() {
+    console.log("- Factory Pattern")
+    console.log("* class-based creational design pattern")
+    console.log("* provide a generic interface that delegates the responsibility of object instantiation to its subclasses")
+    console.log("* frequently used when we need to manage or manipulate collections of objects that are different yet have many similar characteristics")
+    console.log("* EXAMPLE: ")
+
+    class BallGameFactory {
+        constructor() {
+            // remove type to make it more declarative
+            // this.createBall = function(type) {
+            this.createBall = function(TheClass) {
+                let ball;
+
+                // if (type === 'football' || type === 'soccer') {
+                //     ball = new Football();
+                // } else if (type === 'basketball') {
+                //     ball = new Basketball();
+                // }
+
+                ball = new TheClass();
+
+                ball.roll = function() {
+                    return console.log(`The ${this._type} is rolling.`);
+                }
+
+                return ball;
+            };
+        }
+    }
+
+    class Football {
+        constructor() {
+            this._type = 'football';
+            this.kick = function() {
+                return console.log(`You kicked the ${this._type}.`);
+            }
+        }
+    }
+
+    class Basketball {
+        constructor() {
+            this._type = 'basketball';
+            this.bounce = function() {
+                return console.log(`You bounced the ${this._type}.`);
+            };
+        }
+    }
+
+    const theFactory = new BallGameFactory();
+
+    // const soccer = theFactory.createBall('football');
+    // const soccer = theFactory.createBall(Football);
+    const soccer = theFactory.createBall(Football);
+    const basketBall = theFactory.createBall(Basketball);
+
+    soccer.roll();
+    soccer.kick();
+    // soccer.bounce(); // -> soccer.bounce is not a function
+
+    separator("-", 7);
+    basketBall.roll();
 }
