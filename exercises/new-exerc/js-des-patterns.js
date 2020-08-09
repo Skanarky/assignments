cde1f36f82b7d6e44f1c707549008a26ba8e4414
Skanarky <kutkurov@gmail.com>
2020-08-09 11:26:50 -0600

    state patt

--- exercises/new-exerc/js-des-patterns.js
+++ exercises/new-exerc/js-des-patterns.js
@@ -45,7 +45,8 @@ behavioralPattern(
     commandPattern,
     iteratorPattern,
     mediatorPattern,
-    observerPattern
+    observerPattern,
+    statePattern
 );
 
 // - - - - - - -
@@ -988,5 +989,104 @@ function observerPattern() {
 
     console.log(obs1.state);
     console.log(obs2.state); // no change
+
+}
+
+// 3.6.
+
+function statePattern() {
+    console.log("- State Pattern")
+    console.log("* behavioral design pattern that allows an object to alter its behaviour based on changes to its internal state")
+    console.log("* the object returned by a state pattern class seems to change its class")
+    console.log("* it provides state-specific logic to a limited set of objects in which each object type represents a particular state")
+    console.log("* EXAMPLE: ")
+
+    class Light {
+        constructor(light) {
+            this.light = light;
+        }
+
+        sign() {
+            return 'OUT OF SERVICE';
+        }
+    }
+      
+    class RedLight extends Light {
+        constructor() {
+            super('red');
+        }
+
+        sign() {
+            return 'STOP';
+        }
+    }
+
+    class YellowLight extends Light {
+        constructor() {
+            super('yellow');
+        }
+
+        sign() {
+            return 'STEADY';
+        }
+    }
+      
+    class GreenLight extends Light {
+        constructor() {
+            super('green');
+        }
+      
+        sign() {
+            return 'GO';
+        }
+    }
+
+    class OutOfSvcLight extends Light {
+        constructor() {
+            super('blinking yellow');
+        }
+    }
+
+    class TrafficLight {
+        constructor() {
+            this.states = [new RedLight(), new YellowLight(), new GreenLight()];
+            this.current = this.states[0];
+        }
+      
+        change() {
+            const totalStates = this.states.length;
+            let currentIndex = this.states.findIndex(light => light === this.current);
+            if (currentIndex + 1 < totalStates) {
+                this.current = this.states[currentIndex + 1];
+            } else {
+                this.current = this.states[0];
+            }
+        }
+
+        outOfSvc() {
+            this.current = new OutOfSvcLight();
+        }
+      
+        sign() {
+            return this.current.sign();
+        }
+    }
+
+    const trafficLight = new TrafficLight();
+    
+    console.log(trafficLight.sign());
+    trafficLight.change();
+    console.log(trafficLight.sign());
+    trafficLight.change();
+    console.log(trafficLight.sign());
+    trafficLight.outOfSvc();
+    console.log(trafficLight.sign());
+    console.log(trafficLight.sign());
+    trafficLight.change();
+    console.log(trafficLight.sign());
+    trafficLight.change();
+    console.log(trafficLight.sign());
+    trafficLight.change();
+    console.log(trafficLight.sign());
 
 }
